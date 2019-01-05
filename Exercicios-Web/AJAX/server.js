f4eb60efae9ea55ca6c521ca615415ec5a8045c4
GabrielSantos <ghs.developer@outlook.com>
2019-01-05 19:18:00 -0200

    Inclusao de exercicios do modulo de AJAX

---
+++ Exercicios-Web/AJAX/server.js
@@ -0,0 +1,54 @@
+const express = require('express') // Irá receber um conteúdo estático
+const app = express() // const app irá receber a função express()
+const bodyParser = require('body-parser') // transforma o arquivo de texto em um objeto
+
+app.use(express.static('.')) // Caso recebe um dado de um arquivo estático
+app.use(bodyParser.json()) // Caso venha um JSON dentro do body da requisição (transforma JSON em objeto)
+app.use(bodyParser.urlencoded({ extended: true })) // Caso venha algum formato através de uma submit de um formulário
+
+// FUNÇÕES MIDDLEWARE
+
+// Teste para serviço de conteúdo estático
+app.get('/teste', (req, res) => res.send(new Date)) // Caso venha uma requisição na url /teste, retorne a data atual
+
+// Fazendo Upload usando o XMLHttpRequest
+const multer = require('multer')
+
+const storage = multer.diskStorage({ // personalizar o arquivo no momento de salva-lo
+    destination: function (req, file, callback) {
+        callback(null, './upload') // Onde será salvo  
+    }, 
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`) // Será salvo com a data atual_nomeDoArquivo para casos de sobrescrita 
+    }
+})
+
+const upload = multer({ storage }).single('arquivo') // Nome do input que virá na requisição
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('Ocorreu um erro!')
+        }
+        res.end('Concluído com sucesso!')
+    })
+})
+
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body, // terá o retorno de tudo o que está no body...
+        id: 1 // ...Mais o id 1
+    })
+})
+
+app.get('/parOuImpar', (req, res) => {
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
+
+// Setando a porta
+app.listen(8080, () => console.log('Executando...')) // Entrada do servidor na porta 8080 e a execução de inicio
+
+
